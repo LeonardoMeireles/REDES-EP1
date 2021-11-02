a4429bf3a3844f8f1b9a08fc369bfad7f932d456
LeonardoMeireles <leo.meireles@usp.br>
2021-11-02 15:12:32 -0300

    Finished the conflicts ffs

--- src/services/Market.java
+++ src/services/Market.java
@@ -27,7 +27,7 @@ public class Market {
         String price = "";
         String description = "";
         try{
-            client.dataOutputStream.writeUTF("\nProduct name, please keep under 50 characters (Type 'Cancel' to exit):");
+            client.dataOutputStream.writeUTF("\nProduct name, please keep under 50 characters (Type 'cancel' to exit):");
             client.dataOutputStream.flush();
             name = client.dataInputStream.readUTF();
             while(name.length() > 140){
@@ -38,11 +38,11 @@ public class Market {
                     return;
                 }
             }
-            client.dataOutputStream.writeUTF("Description, please keep under 140 characters (Type 'Cancel' to exit):");
+            client.dataOutputStream.writeUTF("Description, please keep under 140 characters (Type 'cancel' to exit):");
             client.dataOutputStream.flush();
             description = client.dataInputStream.readUTF();
             while(description.length() > 140){
-                client.dataOutputStream.writeUTF("Please write a description name under 140 characters (Type 'Cancel' to exit):");
+                client.dataOutputStream.writeUTF("Please write a description name under 140 characters (Type 'cancel' to exit):");
                 client.dataOutputStream.flush();
                 description = client.dataInputStream.readUTF();
                 if(description.toLowerCase().equals("cancel")){
@@ -53,10 +53,10 @@ public class Market {
             String newItemQuery = String.format("INSERT INTO Items (name, description, owner) VALUES('%s', '%s', '%s')", name, description, client.username);
             client.dataOutputStream.writeUTF("Item successfully added, I'll be sure to keep it safe and sound!\n");
             statement.executeUpdate(newItemQuery);
-            statement.close();
             return;
-        } catch (IOException | SQLException error) {
-            error.printStackTrace();
+        } catch (IOException | SQLException | NumberFormatException error) {
+            client.dataOutputStream.writeUTF("Sorry there buddy something went wrong try again.\n");
+            return;
         }
     }
 
@@ -94,18 +94,23 @@ public class Market {
                 }
                 statement.close();
             }
-        } catch(IOException | SQLException error) {
+        } catch(IOException | SQLException | NullPointerException error) {
             error.printStackTrace();
             client.dataOutputStream.writeUTF("Something went wrong my dear friend, please try again.\n");
+            sellItem();
             return;
         }
     }
 
-    public ResultSet getFromStash(String itemId) throws SQLException, IOException { // search for items at item list by item ID
-        statement = connection.createStatement();
-        String findItemQuery = String.format("SELECT * FROM Items WHERE id == '%o';", Integer.parseInt(itemId));
-        ResultSet itemFound = statement.executeQuery(findItemQuery);
-        return itemFound;
+    public ResultSet getFromStash(String itemId) throws SQLException, IOException {
+        try{
+            statement = connection.createStatement();
+            String findItemQuery = String.format("SELECT * FROM Items WHERE id == '%o';", Integer.parseInt(itemId));
+            ResultSet itemFound = statement.executeQuery(findItemQuery);
+            return itemFound;
+        } finally{
+            return null;
+        }
     }
 
     public ResultSet getFromShop(String itemId) throws SQLException, IOException { // search for items at shop list by shop ID
@@ -125,7 +130,7 @@ public class Market {
 
     public void getItem() throws IOException { // get back the item at the market
         try{
-            client.dataOutputStream.writeUTF("What is the id of the item you wish to have back traveler? (type 'List Items' to see all your available items)");
+            client.dataOutputStream.writeUTF("What is item's id you wish to have back traveler? (type 'list items' to see all your available items)");
             String itemId = client.dataInputStream.readUTF();
             if(itemId.toLowerCase().equals("Cancel")){
                 return;
@@ -149,8 +154,9 @@ public class Market {
                     client.dataOutputStream.writeUTF("Sorry there buddy but I couldn't find that item in your stash.\n");
                 }
             }
-        } catch (IOException | SQLException error) {
-            error.printStackTrace();
+        } catch (IOException | SQLException | NumberFormatException error) {
+            client.dataOutputStream.writeUTF("Sorry there buddy something went wrong try again.\n");
+            return;
         }
     }
 /*
@@ -184,8 +190,9 @@ public class Market {
             }
             items.close();
             statement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (IOException | SQLException | NumberFormatException error) {
+            client.dataOutputStream.writeUTF("Sorry there buddy something went wrong try again.\n");
+            return;
         }
     }
 
@@ -220,8 +227,9 @@ public class Market {
             }
             items.close();
             statement.close();
-        } catch (SQLException e) {
-            e.printStackTrace();
+        } catch (IOException | SQLException | NumberFormatException error) {
+            client.dataOutputStream.writeUTF("Sorry there buddy something went wrong try again.\n");
+            return;
         }
     }
 
@@ -280,6 +288,136 @@ public class Market {
             }
         } catch (IOException | SQLException error) {
             error.printStackTrace();
+        }
+    }
+
+    public void bargainItem() throws IOException{
+        try{
+            client.dataOutputStream.writeUTF("What is the shop id for the item you wish bargain for? (type 'list shop' to see all the items up for sale)");
+            String shopId = client.dataInputStream.readUTF();
+            if(shopId.toLowerCase().equals("cancel")){
+                return;
+            } else if(shopId.toLowerCase().equals("list shop")){
+                listShop();
+                bargainItem();
+                return;
+            } else{
+                statement = connection.createStatement();
+                String findItemQuery = String.format("SELECT *, Items.owner  FROM Shop INNER JOIN Items ON Items.id = Shop.itemId WHERE Shop.id == '%o';", Integer.parseInt(shopId));
+                ResultSet itemFound = statement.executeQuery(findItemQuery);
+                if(itemFound.next()){
+                    if(itemFound.getString("owner").equals(client.username)){
+                        client.dataOutputStream.writeUTF("Sorry but you can only counter-offer to your own items.\n");
+                        return;
+                    } else{
+                        client.dataOutputStream.writeUTF("How much are you offering to buy the item for?");
+                        String offer = client.dataInputStream.readUTF();
+                        client.dataOutputStream.writeUTF("Would you like to leave a message?\nY: yes\tN: no");
+                        String answer = client.dataInputStream.readUTF();
+                        String message = null;
+                        if(answer.toLowerCase().equals("y") || answer.toLowerCase().equals("yes")){
+                            client.dataOutputStream.writeUTF("Please leave a message for the owner:");
+                            message = client.dataInputStream.readUTF();
+                        }
+                        String findBargain = String.format("SELECT * FROM Bargain WHERE buyerUsername == '%s' AND shopId == '%o';", client.username, Integer.parseInt(shopId));
+                        ResultSet bargainFound = statement.executeQuery(findBargain);
+                        if(bargainFound.next()){
+                            String updateOfferQuery = String.format("UPDATE Bargain SET offer = '%.2f' WHERE id == '%o';", Float.parseFloat(offer), bargainFound.getInt("id"));
+                            statement.executeUpdate(updateOfferQuery);
+                        } else{
+                            String newOfferQuery = String.format("INSERT INTO Bargain (shopId, buyerUsername, offer, message) VALUES('%o', '%s', '%.2f', '%s')", Integer.parseInt(shopId), client.username, Float.parseFloat(offer), message);
+                            statement.executeUpdate(newOfferQuery);
+                        }
+                        client.dataOutputStream.writeUTF("Your offer was sent!\n");
+                        statement.close();
+                    }
+                    return;
+                } else{
+                    client.dataOutputStream.writeUTF("Sorry there buddy but I couldn't find that item in the shop.\n");
+                }
+            }
+        } catch (IOException | SQLException | NumberFormatException error) {
+            client.dataOutputStream.writeUTF("Sorry there friend, it appears you wrote the wrong input.\n");
+        }
+    }
+
+    public void listOffers() throws IOException{
+        try {
+            statement= connection.createStatement();
+            ResultSet offers = statement.executeQuery("SELECT Bargain.id, Items.name, Items.description, Shop.price, Bargain.buyerUsername, Bargain.offer, Bargain.message FROM Bargain INNER JOIN Shop ON Shop.id = Bargain.shopId INNER JOIN Items ON Items.id = Shop.itemId WHERE Items.owner == '" +client.username + "';");
+            StringBuilder text = new StringBuilder();
+            text.append("Offers: \n\n\n");
+            if (offers.isBeforeFirst()){
+                while(offers.next()) {
+                    int bargainId = offers.getInt("id");
+                    String itemName = offers.getString("name");
+                    String itemPrice = offers.getString("price");
+                    String buyerUsername = offers.getString("buyerUsername");
+                    Float offer = offers.getFloat("offer");
+                    String message = offers.getString("message");
+
+                    text.append("Id: " + bargainId +"\n");
+                    text.append("Item name: " + itemName + " || Price listed: " +itemPrice +"\n\n");
+                    text.append("Buyer: "+ buyerUsername + " || Offer: " +offer +" gold\n");
+                    text.append("Message: "+ message + "\n");
+                    text.append("------------------------------------------------------------------------------------------------------------------------------------------------------------\n");
+                }
+                client.dataOutputStream.writeUTF(text.toString());
+            } else{
+                client.dataOutputStream.writeUTF("You haven't received any offers.\n");
+            }
+            offers.close();
+            statement.close();
+        } catch (IOException | SQLException | NumberFormatException error) {
+            client.dataOutputStream.writeUTF("Sorry there buddy something went wrong try again.\n");
+            return;
+        }
+    }
+
+    public void counterOffer() throws IOException{
+        try{
+            client.dataOutputStream.writeUTF("What is the offer's id you wish to counteroffer? (type 'list offers' to see all the items up for sale)");
+            String offerId = client.dataInputStream.readUTF();
+            if(offerId.toLowerCase().equals("cancel")){
+                return;
+            } else if(offerId.toLowerCase().equals("list offers")){
+                listOffers();
+                counterOffer();
+                return;
+            } else{
+                statement = connection.createStatement();
+                String findOfferQuery = String.format("SELECT Bargain.id, Items.owner FROM Bargain INNER JOIN Shop ON Shop.id = Bargain.shopId INNER JOIN Items ON Items.id = Shop.itemId WHERE Bargain.id == '%s' AND Items.owner == '%s';", Integer.parseInt(offerId), client.username);
+                ResultSet offerFound = statement.executeQuery(findOfferQuery);
+                if(offerFound.next()){
+                    int offerFoundId = offerFound.getInt("id");
+                    client.dataOutputStream.writeUTF("What is your counteroffer?");
+                    String counterOffer = client.dataInputStream.readUTF();
+                    client.dataOutputStream.writeUTF("Would you like to leave a message?\nY: yes\tN: no");
+                    String answer = client.dataInputStream.readUTF();
+                    String message = null;
+                    if(answer.toLowerCase().equals("y") || answer.toLowerCase().equals("yes")){
+                        client.dataOutputStream.writeUTF("Please leave a message for the buyer:");
+                        message = client.dataInputStream.readUTF();
+                    }
+                    String findCounterOfferQuery = String.format("SELECT * FROM CounterOffer WHERE bargainId == '%o';", offerFoundId);
+                    ResultSet counterOfferFound = statement.executeQuery(findCounterOfferQuery);
+                    if(counterOfferFound.next()){
+                        String updateCounterOfferQuery = String.format("UPDATE CounterOffer SET counterOffer = '%.2f' WHERE id == '%o';", Float.parseFloat(counterOffer), counterOfferFound.getInt("id"));
+                        statement.executeUpdate(updateCounterOfferQuery);
+                    } else{
+                        String newCounterOfferQuery = String.format("INSERT INTO CounterOffer (bargainId, counterOffer, message) VALUES('%o', '%.2f', '%s')", offerFoundId, Float.parseFloat(counterOffer), message);
+                        statement.executeUpdate(newCounterOfferQuery);
+                    }
+                    client.dataOutputStream.writeUTF("Your counteroffer was sent!\n");
+                    statement.close();
+                } else{
+                    client.dataOutputStream.writeUTF("Sorry there buddy but that id wasn't valid.\n");
+                }
+            }
+            statement.close();
+        } catch (IOException | SQLException | NumberFormatException error) {
+            client.dataOutputStream.writeUTF("Sorry there buddy something went wrong try again.\n");
+            return;
         }
     }
 
